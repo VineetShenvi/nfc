3073e43bba7d7cb2a61aa76e9f3cf7a6e1901027
VineetShenvi <vinshenvi2004@gmail.com>
2024-08-30 03:04:50 +0530

    speech to text

--- api/chat.py
+++ api/chat.py
@@ -1,12 +1,15 @@
 import os
+import tempfile
+from pathlib import Path
 from flask import Flask, request, jsonify
-from langchain_openai import OpenAI
+import langchain_openai 
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from dotenv import load_dotenv
 from flask_cors import CORS
+import openai
 from prompt import *
 
 load_dotenv()
@@ -17,7 +20,8 @@ load_dotenv()
 PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 chain_type_kwargs = {"prompt": PROMPT}
 
-llm = OpenAI(openai_api_key=os.environ["OPENAI_API_KEY"], temperature=0.9, max_tokens=2000, model="gpt-3.5-turbo-instruct")
+llm = langchain_openai.OpenAI(openai_api_key=os.environ["OPENAI_API_KEY"], temperature=0.9, max_tokens=2000, model="gpt-3.5-turbo-instruct")
+client = openai.OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 new_vector_store = FAISS.load_local(
     "faiss_index", embeddings=OpenAIEmbeddings(), allow_dangerous_deserialization=True
@@ -30,6 +34,8 @@ qa = RetrievalQA.from_chain_type(
     return_source_documents=True, 
     chain_type_kwargs=chain_type_kwargs
 )
+
+temp_folder = tempfile.mkdtemp()
 
 app = Flask(__name__)
 CORS(app)
@@ -44,6 +50,26 @@ def chat():
     # Perform the query using the RAG model
     result = qa({"query": query})
     return jsonify({"result": result["result"]})
+
+@app.route("/api/transcribe", methods=["POST"])
+def transcribe():
+    print("hi")
+    try:
+        audio_file = request.files['audio']
+        if audio_file and audio_file.filename.endswith(('.mp3', '.wav', '.flac', '.mp4')):
+            audio_path = os.path.join(temp_folder, audio_file.filename)
+            audio_file.save(audio_path)
+            transcript = client.audio.translations.create(
+                model="whisper-1",
+                file=Path(audio_path),
+                response_format="text"
+            )
+            return jsonify({"transcript": transcript})
+
+        else:
+            return jsonify({"error": "Invalid audio file format"}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
     
