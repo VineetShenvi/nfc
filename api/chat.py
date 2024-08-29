2a9c9f0d8fd9f2aa63aaf9aaf476ef8a1b977ee0
VineetShenvi <vinshenvi2004@gmail.com>
2024-08-30 00:47:45 +0530

    kjbkjb

--- api/chat.py
+++ api/chat.py
@@ -48,4 +48,5 @@ def chat():
 
     
 if __name__ == '__main__':
-    app.run(port=os.environ["PORT"], debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
